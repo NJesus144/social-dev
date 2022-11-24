7b1b161c97873cec1d05454b80f7756c26379ad5
Nalbert de Jesus <nalbert-edm@outlook.com>
2022-11-24 04:25:15 -0300

    Criado signup e utilizado virtualDom

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -17,28 +17,33 @@ const Form = styled.form`
   flex-direction: column;
   margin: 20px 0;
   gap: 20px;
-`
+`;
 
 const Text = styled.p`
   text-align: center;
-`
+`;
 
-function LoginPage () {
+function SignupPage() {
   return (
     <ImageWithSpace>
       <H1> # Social Dev</H1>
       <H4>Tudo que acontence no mundo dev, está aqui!</H4>
       <FormContainer>
-        <H2>Entre em sua conta</H2>
+        <H2>Crie sua conta</H2>
         <Form>
-          <Input label="Email ou usuário" type="email"/>
+          <Input label="Nome" />
+          <Input label="Sobrenome" />
+          <Input label="Usuário" />
+          <Input label="Email ou usuário" type="email" />
           <Input label="Senha" type="password" />
           <Button>Entrar</Button>
         </Form>
-        <Text>Não possui uma conta? <Link href="/signup">Faça seu cadastro</Link>  </Text>
+        <Text>
+          Já possui uma conta? <Link href="/login">Faça seu login</Link>
+        </Text>
       </FormContainer>
     </ImageWithSpace>
-  )
+  );
 }
 
-export default LoginPage;
+export default SignupPage;

f8287a9daab6465a02ff245d1f1c9fcd9035054a
Nalbert de Jesus <nalbert-edm@outlook.com>
2022-11-27 19:15:27 -0300

    Criado tratamento de erros para o cadastro

--- src/components/inputs/Button.jsx
+++ src/components/inputs/Button.jsx
@@ -1,18 +1,23 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  background-color: ${props => props.theme.primary};
+  background-color: ${(props) => props.theme.primary};
   padding: 15px 20px;
   border-radius: 10px;
   border: 0;
   font-weight: bold;
-  color: ${props => props.theme.white};
+  color: ${(props) => props.theme.white};
   font-size: 16px;
   cursor: pointer;
   transition: 0.3s;
-  
+
   :hover {
-    background-color: ${props => props.theme.primaryHover};
+    background-color: ${(props) => props.theme.primaryHover};
+  }
+
+  :disabled {
+    background-color: ${(props) => props.theme.disabled};
+    cursor: default;
   }
 `;
 

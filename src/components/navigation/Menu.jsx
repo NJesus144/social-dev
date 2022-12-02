68f7f5f05b671be4bb9ba002da6426466f22d051
Nalbert de Jesus <nalbert-edm@outlook.com>
2022-12-02 05:24:28 -0300

    Criado menu para posts

---
+++ src/components/navigation/Menu.jsx
@@ -0,0 +1,74 @@
+import { useState, useRef, useEffect } from "react";
+import styled from "styled-components";
+
+const Dots = styled.img`
+  cursor: pointer;
+`;
+
+const StyledMenu = styled.div`
+  position: absolute;
+  width: 200px;
+  box-shadow: 6px 5px 15px 5px rgba(0, 0, 0, 0.15);
+  right: 0;
+  background-color: ${(props) => props.theme.white};
+
+  display: ${(props) => (props.show ? "block" : "none")};
+`;
+
+const StyledOption = styled.div`
+  padding: 15px;
+  cursor: pointer;
+
+  :hover {
+    background-color: ${(props) => props.theme.background};
+  }
+`;
+
+const StyleContainerMenu = styled.div`
+  position: relative;
+`;
+
+const Menu = ({ options=[] }) => {
+  const [show, setShow] = useState(false);
+  const menuRef = useRef(null);
+
+  const handleDots = () => {
+    setShow(!show);
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if(menuRef.current && !menuRef.current.contains(event.target)) {
+        setShow(false)
+      } 
+    }
+    document.addEventListener('click', handleClickOutside, true)
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true)
+    }
+
+  }, [menuRef])
+
+  return (
+    <StyleContainerMenu>
+      <Dots src="/three-dots.svg" height="20" onClick={handleDots}/>
+      <StyledMenu show={show} ref={menuRef} onBlur={() => setShow(false)}>
+        
+       {
+        options.map((option, pos) => 
+          <StyledOption 
+          key={`menu-option-${pos}`}
+          onClick={option.onClick}
+          >
+            {option.text}
+          </StyledOption>
+        )
+       }
+      
+      </StyledMenu>
+    </StyleContainerMenu>
+  );
+};
+
+export default Menu;
